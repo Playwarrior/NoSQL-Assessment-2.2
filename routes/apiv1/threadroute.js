20db6c546ca2d98ad9d70a62c42097a4e1e9aa6b
robin <playwarrior2@gmail.com>
2019-11-11 10:36:06 +0100

    Updated GET-Routes

--- routes/apiv1/threadroute.js
+++ routes/apiv1/threadroute.js
@@ -7,8 +7,26 @@ router.get('', (req, res, next) => {
     Thread.find().then((threads) => {
         res.status(200).json(threads);
     }).catch((error) => {
-       next(error);
+        next(error);
     });
 });
+
+router.get('/:id', (req, res, next) => {
+    Thread.find({_id: req.params.id}).then((thread) => {
+        res.status(200).json(thread);
+    }).catch((error) => {
+        next(error);
+    });
+});
+
+router.get('/:id/comments', (req, res, next) => {
+   Comment.find({threadId: req.params.id}).then((comments) => {
+       res.status(200).json(comments);
+   }).catch((error) => {
+       next(error);
+   });
+});
+
+
 
 module.exports = router;
