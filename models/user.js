f51407c1914644dc7efe93e57c69ce85a13a1a05
Brion <khobryan2000@gmail.com>
2019-11-13 22:30:28 +0100

    Added validation

--- models/user.js
+++ models/user.js
@@ -1,11 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    userName: String,
-    password: String,
-    registerDate: Date
-});
+const UserSchema = new Schema(
+	{
+		userName: {
+			type: String,
+			validate: {
+				validator: function(v) {
+					return /^[a-zA-Z0-9_-]{3,20}$/.test(v);
+				},
+				message: (props) => `${props.value} is not a valid userName!`
+			},
+			required: [ true, 'userName is required (min. 3 tokens, max. 20 tokens)' ],
+			unique: true
+		},
+		password: {
+			type: String,
+			required: [
+				true,
+				'Password is required (Should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long)'
+			]
+		},
+		registerDate: {
+			type: Date,
+			default: Date.now()
+		}
+	},
+	{ collection: 'users' }
+);
 
 const User = mongoose.model('user', UserSchema);
 
