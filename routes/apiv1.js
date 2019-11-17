66f1564d4d36be8758230168b8438854391c0c37
Brion <khobryan2000@gmail.com>
2019-11-17 23:05:02 +0100

    Added route for friendships

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const commentRoute = require('./apiv1/commentroute');
 const userRoute = require('./apiv1/userroute');
 const threadRoute = require('./apiv1/threadroute');
+const friendshipRoute = require('./apiv1/friendshipRoute');
 
 router.use('/threads', threadRoute);
 router.use('/users', userRoute);
 router.use('/comments', commentRoute);
+router.use('/friendship', friendshipRoute);
 
 module.exports = router;
